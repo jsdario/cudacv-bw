324bc32b0d9e49ea4dfe7c66f3b7441ca77cef3b
jesusdario <jesusdriru@gmail.com>
2015-02-10 12:05:22 +0100

    Changes thread per block n'grid strategy

--- rgba_to_grey.cu
+++ rgba_to_grey.cu
@@ -1,14 +1,16 @@
 #include "utils.h"
 #include <stdio.h>
+#include <math.h>       /* ceil */
+
+// Max Threads per block in GeForce 210
+#define TxB 512
 
 __global__
 void rgba_to_grey_kernel(const uchar4* const rgbaImage,
                        unsigned char* const greyImage,
                        int numRows, int numCols)
 {
-  //TODO
-  //Fill in the kernel to convert from color to greyscale
-  //the mapping from components of a uchar4 to RGBA is:
+  // The mapping from components of a uchar4 to RGBA is:
   // .x -> R ; .y -> G ; .z -> B ; .w -> A
   //
   //The output (greyImage) at each pixel should be the result of
@@ -19,19 +21,19 @@ void rgba_to_grey_kernel(const uchar4* const rgbaImage,
   greyImage[i] = .299f * px.x +
                  .587f * px.y +
                  .114f * px.z;
-    
-  //First create a mapping from the 2D block and grid locations
-  //to an absolute 2D location in the image, then use that to
-  //calculate a 1D offset
 }
 
 void rgba_to_grey(uchar4 * const d_rgbaImage,
                   unsigned char* const d_greyImage, size_t numRows, size_t numCols)
 {
-  //You must fill in the correct sizes for the blockSize and gridSize
-  //currently only one block with one thread is being launched
-  const dim3 blockSize(512, 1, 1);  //TODO
-  const dim3 gridSize(512, 1, 1);  //TODO
+
+  // Since it does not matter the relative position of a pixel
+  // the block - grid assign strategy will simply be to cover
+  // all pixels secuencially in 'x' axis
+  long long int total_px = numRows * numCols;  // total pixels
+  long int grids_n = ceil(total_px / TxB); // grids numer
+  const dim3 blockSize(TxB, 1, 1);
+  const dim3 gridSize(grids_n, 1, 1);
   rgba_to_grey_kernel<<<gridSize, blockSize>>>(d_rgbaImage, d_greyImage, numRows, numCols);
   
   cudaDeviceSynchronize(); checkCudaErrors(cudaGetLastError());
